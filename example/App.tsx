0016a4ac5d500a4893bff5adfb23617c79138553
Ryan Graff <ryantgraff@resistdesign.com>
2021-02-13 17:29:37 -0500

    feat: (example) Add more component types

--- example/App.tsx
+++ example/App.tsx
@@ -7,11 +7,43 @@ const Identifier = styled.div`
   background-color: darkorchid;
   color: white;
 `;
+const AssignmentPattern = styled.div`
+  background-color: aqua;
+`;
+const ExportDefaultDeclaration = styled.div`
+  background-color: darkorange;
+  color: white;
+`;
+const ClassProperty = styled.div`
+  background-color: chocolate;
+  color: yellow;
+`;
+const NoComp = () => null;
 
 const COMPONENT_MAP: { [type: string]: SuperficialFactoryComponent | undefined } = {
-  Identifier: ({ name, children }) => (
-    <Identifier data-name={name} data-type="Identifier">
+  Program: ({ data }) => <Superficial name="body" data={data?.body} componentFactory={componentFactory} />,
+  ImportDeclaration: NoComp,
+  VariableDeclaration: NoComp,
+  ExportDefaultDeclaration: ({ name, data, children }) => (
+    <ExportDefaultDeclaration data-name={name} data-type="AssignmentPattern">
+      Default: {data?.declaration?.id?.name}
       {children}
+    </ExportDefaultDeclaration>
+  ),
+  ClassProperty: ({ name, data, children }) => (
+    <ClassProperty data-name={name} data-type="AssignmentPattern">
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      {children}
+    </ClassProperty>
+  ),
+  AssignmentPattern: ({ name, children }) => (
+    <AssignmentPattern data-name={name} data-type="AssignmentPattern">
+      {children}
+    </AssignmentPattern>
+  ),
+  Identifier: ({ name, data }) => (
+    <Identifier data-name={name} data-type="Identifier">
+      {data?.name}
     </Identifier>
   ),
 };
