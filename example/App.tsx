ab221302f6c3ea471773da58e15c3c57f6d858c5
Ryan Graff <ryantgraff@resistdesign.com>
2021-02-13 23:47:05 -0500

    feat: (example) Add AST exploration utils

--- example/App.tsx
+++ example/App.tsx
@@ -3,6 +3,70 @@ import SampleAST from './SampleAST';
 import Superficial, { SuperficialComponentFactory, SuperficialFactoryComponent } from '../src';
 import styled from 'styled-components';
 
+type ASTInfo = {
+  type: string;
+  [key: string]: any;
+};
+type APISurfaceMap = {
+  defaultExport?: ASTInfo;
+  namedExports: ASTInfo[];
+};
+type ASTTypeInfoTransformMap = {
+  [astType: string]: (astData: ASTInfo, map: APISurfaceMap, populateMap: (astData: any) => void) => void;
+};
+
+const getASTDateType = (astData: any, expand?: boolean, mergeObjectData?: { [key: string]: any }): any => {
+  if (astData instanceof Array) {
+    return astData.map((item) => getASTDateType(item)).reduce((acc, item) => (acc.indexOf(item) === -1 ? [...acc, item] : acc), []);
+  } else if (astData instanceof Object) {
+    if (typeof astData.type === 'string' && !expand) {
+      return astData.type;
+    } else if (astData.constructor && astData.constructor !== Object && typeof astData.constructor.name === 'string') {
+      return astData.constructor.name;
+    } else {
+      const merged = {
+        ...mergeObjectData,
+        ...astData,
+      };
+
+      return Object.keys(merged).reduce((acc, k) => ({ ...acc, [k]: getASTDateType(merged[k]) }), {});
+    }
+  } else if (astData === null) {
+    return 'null';
+  } else {
+    return typeof astData;
+  }
+};
+
+const API_SURFACE_MAP: APISurfaceMap = {
+  namedExports: [],
+};
+const TYPE_COLLECTION_MAP: { [type: string]: any } = {};
+const AST_TYPE_INFO_TRANSFORM_MAP: ASTTypeInfoTransformMap = {
+  ExportDefaultDeclaration: (astData, map, populateMap) => {
+    map.defaultExport = astData;
+
+    populateMap(astData.declaration);
+  },
+};
+const populateAPISurfaceMap = (astData: any) => {
+  if (astData instanceof Array) {
+    astData.forEach((item) => populateAPISurfaceMap(item));
+  } else if (astData instanceof Object) {
+    if (typeof astData.type === 'string') {
+      TYPE_COLLECTION_MAP[astData.type] = getASTDateType(astData, true, TYPE_COLLECTION_MAP[astData.type]);
+    }
+
+    if (typeof astData.type === 'string' && AST_TYPE_INFO_TRANSFORM_MAP[astData.type] instanceof Function) {
+      AST_TYPE_INFO_TRANSFORM_MAP[astData.type](astData, API_SURFACE_MAP, populateAPISurfaceMap);
+    }
+
+    Object.keys(astData).forEach((k) => populateAPISurfaceMap(astData[k]));
+  }
+};
+
+populateAPISurfaceMap(SampleAST);
+
 const Identifier = styled.div`
   background-color: darkorchid;
   color: white;
@@ -62,7 +126,13 @@ const COMPONENT_MAP: { [type: string]: SuperficialFactoryComponent | undefined }
 const componentFactory: SuperficialComponentFactory = ({ data }) => data && typeof data.type === 'string' && COMPONENT_MAP[data.type];
 
 export const App: FC = () => {
-  return <Superficial name="Base" data={SampleAST} componentFactory={componentFactory} />;
+  return (
+    <>
+      <pre>{JSON.stringify(TYPE_COLLECTION_MAP, null, 2)}</pre>
+      <pre>{JSON.stringify(API_SURFACE_MAP, null, 2)}</pre>
+    </>
+  );
+  // return <Superficial name="Base" data={SampleAST} componentFactory={componentFactory} />;
 };
 
 export default App;
